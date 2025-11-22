7ede03dae81b71b47f54386cbd309da383e07cbd
Oumaima <chellyoumaima70@gmail.com>
2025-11-22 13:56:09 +0100

    feat: microservice mobilityService initialized

---
+++ mobilityService/src/main/java/com/smartCity/mobilityService/MobilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartCity.mobilityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilityServiceApplication.class, args);
+	}
+
+}
---
+++ mobilityService/src/main/java/com/smartCity/mobilityService/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.smartCity.mobilityService;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobilityServiceApplication.class);
+	}
+
+}
